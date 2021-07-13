19ebe6de00c7f02af27cd3cb5653ec3b9f9873bc
Dilleys <dilleys@gmail.com>
2021-07-12 21:09:47 -0300

    correções de bugs

--- src/BuscaForm.js
+++ src/BuscaForm.js
@@ -7,6 +7,7 @@ import { movies } from './Data/movies';
 import { filter } from 'minimatch';
 import { Link } from 'react-router-dom';
 import { api } from './services';
+import { RepeatOneSharp } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
 	form: {
@@ -42,13 +43,28 @@ export default function BuscaForm() {
   const [initialMovies,setinitialMovies] = useState(movies)
   const [error, setError]= useState(false)
 
+  const removeDuplicated = (array) => {
+	const filteredArr = array.reduce((accumulator, current) => {
+	  const result = accumulator.find(item => item.Tile === current.Title);
+	  if (!result) {
+		  return accumulator.concat([current]);
+		} else {
+		  return accumulator;
+		}
+	  }, []);
+	  return filteredArr;
+  }
+
   const getMoviesFromApi =(event) =>{
 	  event.preventDefault();
 	  if(search.length>2){
 		  setError(false)
 		api.get(`/?apikey=67f2f4c&&s=${search}`).then((response)=>{
-		setMoviesFiltered(response.data.Search);
-		setinitialMovies([...initialMovies,...response.data.Search])
+		  if(response.data.Response!= "False"){
+			setMoviesFiltered(response.data.Search);
+			setinitialMovies(removeDuplicated([...initialMovies,...response.data.Search]));
+		  }
+		  
    });
   }
   else{
